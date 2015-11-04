532ecdb806ffa7db645d762dab6a78d8313d5654
bgorla <bgorla@walmartlabs.com>
2015-11-03 16:35:10 -0800

    Adding truncating script to my repo

---
+++ truncate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+LOG_DIR="/root/PBSpectrum/server/app/repository/logs"
+cd $LOG_DIR
+
+#truncating all the log files
+for logs in `ls *.log`;do
+    /usr/bin/truncate -s 0 ${logs}
+done
